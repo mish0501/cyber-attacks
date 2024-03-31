21b9bac2c61e1200d1236a6571d99ea6f050c090
Mihail Georgiev <mmisho0501@gmail.com>
2024-03-31 22:20:25 +0300

    Force dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-import forms from "@tailwindcss/forms";
-import flowbite from "flowbite/plugin";
+import defaultTheme from 'tailwindcss/defaultTheme'
+import forms from '@tailwindcss/forms'
+import flowbite from 'flowbite/plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./node_modules/flowbite/**/*.js",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-        "./resources/js/**/*.vue",
-    ],
+  darkMode: 'class',
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './node_modules/flowbite/**/*.js',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.vue',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
     },
+  },
 
-    plugins: [forms, flowbite],
-};
+  plugins: [forms, flowbite],
+}
